bb830755e914358bd7cb8a7e5210467a5851d338
BabaYaGa74 <beeplovenepal74@gmail.com>
2024-05-24 16:37:01 +0545

    add config

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: "social_media",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
