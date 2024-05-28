cc04430c38a4d59f4afb3dbced0b4a0d2b2d1193
BabaYaGa74 <beeplovenepal74@gmail.com>
2024-05-28 19:07:34 +0545

    add auth Middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const UserQuery = require("../models/userQuery");
+
+const protect = async (req, res, next) => {
+  let token;
+  token = req.cookies.jwt;
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await UserQuery.getUserById(decoded.userId);
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, Invalid token");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, No token");
+  }
+};
+
+module.exports = protect;
