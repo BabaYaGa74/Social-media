58ee2562e579f9794d040d83bd1128934411dfd7
BabaYaGa74 <beeplovenepal74@gmail.com>
2024-06-07 18:32:30 +0545

    fixed

--- backend/controllers/likeController.js
+++ backend/controllers/likeController.js
@@ -1,38 +1,60 @@
 const asyncHandler = require("express-async-handler");
-const LikesQuery = require("../models/likesQuery");
+const LikesQuery = require("../queries/likesQuery");
 
 //@desc   Likes the post
 //@routes POST /api/like/:postId/like
 //@access private
 const likePost = asyncHandler(async (req, res) => {
   const { postId } = req.params;
-  const userId = req.user._id;
-  console.log(userId);
+  const userId = req.user.id;
   const liked = await LikesQuery.userLiked(userId, postId);
   if (!liked) {
     const inserted = await LikesQuery.insertLike(userId, postId)
-    if(inserted) res.json({message:"POST Liked!"})
+    if(inserted){
+      const total_likes = await LikesQuery.likesCount(postId);
+      res.json({ likesCount: total_likes });
+    } 
   }
   const total_likes = await LikesQuery.likesCount(postId);
   res.json({ likesCount: total_likes });
 });
+
 
 //@desc   Unlikes the post
 //@routes Delete /api/like/:postId/unlike
 //@access private
 const unlikePost = asyncHandler(async (req, res) => {
   const { postId } = req.params;
-  const userId = req.user._id;
+  const userId = req.user.id;
   const liked = await LikesQuery.userLiked(userId, postId);
   if (liked) {
-    const removed = await LikesQuery.remove(userId, postId)
-    if(removed) res.json({message:"POST UnLiked!"})
-  }
+    const removed = await LikesQuery.removeLike(userId, postId)
+    const total_likes = await LikesQuery.likesCount(postId);
+    if(removed) res.json({likesCount: total_likes})
+  }else{
+    const total_likes = await LikesQuery.likesCount(postId);
+    res.json({ likesCount: total_likes });
+}
+});
+
+
+//@desc   Gives the likes details of post
+//@routes Delete /api/like/:postId/detail
+//@access private
+const likesDetails = asyncHandler(async(req, res)=> {
+  const {postId} = req.params;
+  const userId = req.user.id;
   const total_likes = await LikesQuery.likesCount(postId);
-  res.json({ likesCount: total_likes });
+  const liked = await LikesQuery.userLiked(userId, postId);
+  if(liked) {
+    res.json({likesCount: total_likes, liked: liked})
+  }else{
+    res.json({ likesCount: total_likes, liked: liked });
+  }
 });
 
 module.exports = {
   likePost,
   unlikePost,
+  likesDetails
 };
